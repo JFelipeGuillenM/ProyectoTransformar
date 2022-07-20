3e02f99db77cbdd82810a9ddda75950a108810dc
JFelipeGuillenM <jose.guillenm@ug.edu.ec>
2022-07-20 18:50:58 -0500

    preguntas aleatorias, correción en timer

--- app/src/main/java/com/example/transformar/QuizLogico.java
+++ app/src/main/java/com/example/transformar/QuizLogico.java
@@ -3,6 +3,7 @@ package com.example.transformar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.TotalCaptureResult;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ import com.google.protobuf.StringValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class QuizLogico extends AppCompatActivity {
 
@@ -30,12 +33,18 @@ public class QuizLogico extends AppCompatActivity {
     int puntajeValue = 0;
     int vidaInicial = 3;
     private ProgressBar progressBar;
-    private TextView puntaje, pregunta;
+    private TextView puntaje, pregunta, preguntaAct;
     private RadioButton rb_op1, rb_op2, rb_op3, rb_op4;
+    private RadioGroup rbGroup;
     private ImageView vidas;
+    private Button sgtePregunta;
     private List<ListaPreguntas> listaPreguntas = new ArrayList<>();
     private int preguntaActual = 0;
-
+    private String itemSelected = "";
+    private int timerValue = 100;
+    private int totalPreguntas = 10;
+    private int totalPantalla = 10;
+    private int currentQuestion = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +55,7 @@ public class QuizLogico extends AppCompatActivity {
         rb_op2 = (RadioButton)findViewById(R.id.rb_op2);
         rb_op3 = (RadioButton)findViewById(R.id.rb_op3);
         rb_op4 = (RadioButton)findViewById(R.id.rb_op4);
+
 
         puntaje = (TextView)findViewById(R.id.txt_puntaje);
         puntaje.setText(String.valueOf(puntajeValue));
@@ -58,9 +68,14 @@ public class QuizLogico extends AppCompatActivity {
     }
 
     public void iniciarContador(){
+        timerValue = 100;
         progressBar = (ProgressBar)findViewById(R.id.barraTiempo);
+
+        if(countDownTimer!=null){
+            countDownTimer.cancel();
+        }
+
         countDownTimer = new CountDownTimer(10000, 100) {
-        int timerValue = 100;
             @Override
             public void onTick(long millisUntilFinished) {
                 timerValue = timerValue-1;
@@ -68,7 +83,9 @@ public class QuizLogico extends AppCompatActivity {
             }
             @Override
             public void onFinish() {
-                Toast.makeText(QuizLogico.this, "Has perdido una vida", Toast.LENGTH_SHORT).show();
+                if(vidaInicial>=2){
+                    Toast.makeText(QuizLogico.this, "Has perdido una vida", Toast.LENGTH_SHORT).show();
+                }
                 vidaInicial--;
                 setVida();
                 iniciarContador();
@@ -93,12 +110,15 @@ public class QuizLogico extends AppCompatActivity {
 
     public void setPreguntas(){
         //Obtener preguntas desde firebase
+        preguntaAct = (TextView)findViewById(R.id.txt_preguntaActual);
+        preguntaAct.setText(String.valueOf(currentQuestion)+"/"+String.valueOf(totalPantalla));
+        iniciarContador();
+        sgtePregunta = (Button)findViewById(R.id.btnSgtePregunta);
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://transformar-feb68-default-rtdb.firebaseio.com/");
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                iniciarContador();
 
                 //Obteniendo todas las preguntas de firebase del tipo lógico
                 for(DataSnapshot dataSnapshot : snapshot.child("logico").getChildren()){
@@ -114,11 +134,39 @@ public class QuizLogico extends AppCompatActivity {
                     listaPreguntas.add(listaPregunta);
                 }
 
+                Random rand = new Random();
+                preguntaActual = rand.nextInt(listaPreguntas.size()) + 1;
+
                 pregunta.setText(listaPreguntas.get(preguntaActual).getPregunta());
                 rb_op1.setText(listaPreguntas.get(preguntaActual).getOp1());
                 rb_op2.setText(listaPreguntas.get(preguntaActual).getOp2());
                 rb_op3.setText(listaPreguntas.get(preguntaActual).getOp3());
                 rb_op4.setText(listaPreguntas.get(preguntaActual).getOp4());
+
+                sgtePregunta.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        currentQuestion++;
+                        if(totalPreguntas == 1){
+                            finish();
+                        }
+                        else if(rb_op1.isChecked()) {
+                            itemSelected = listaPreguntas.get(preguntaActual).getOp1();
+                            verificarRespuesta(itemSelected, listaPreguntas.get(preguntaActual).getRespuesta());
+                        } else if(rb_op2.isChecked()) {
+                            itemSelected = listaPreguntas.get(preguntaActual).getOp2();
+                            verificarRespuesta(itemSelected, listaPreguntas.get(preguntaActual).getRespuesta());
+                        } else if(rb_op3.isChecked()) {
+                            itemSelected = listaPreguntas.get(preguntaActual).getOp3();
+                            verificarRespuesta(itemSelected, listaPreguntas.get(preguntaActual).getRespuesta());
+                        } else if(rb_op4.isChecked()) {
+                            itemSelected = listaPreguntas.get(preguntaActual).getOp4();
+                            verificarRespuesta(itemSelected, listaPreguntas.get(preguntaActual).getRespuesta());
+                        } else {
+                            Toast.makeText(v.getContext(), "Debe seleccionar una opción", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
 
             @Override
@@ -126,6 +174,26 @@ public class QuizLogico extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void verificarRespuesta(@NonNull String selected, String respuesta){
+        rbGroup = (RadioGroup)findViewById(R.id.radioGroupOpciones);
+        if(selected.equals(respuesta)){
+            preguntaActual++;
+            countDownTimer.cancel();
+            setPreguntas();
+            puntajeValue = puntajeValue+10;
+            puntaje.setText(String.valueOf(puntajeValue));
+            rbGroup.clearCheck();
+            totalPreguntas--;
+        }else{
+            preguntaActual++;
+            vidaInicial--;
+            setVida();
+            setPreguntas();
+            rbGroup.clearCheck();
+            totalPreguntas--;
+        }
     }
 
 }
